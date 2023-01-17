10e8880795b116533ea0ae1010f266f223eb44e9
Michael Cousins <michael.cousins@seeingmachines.com>
2023-01-18 10:52:27 +1100

    add plugins to wsl setup

--- setup-wsl.sh
+++ setup-wsl.sh
@@ -11,6 +11,10 @@ sudo apt update && sudo apt install -y \
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
+
 # Change default shell to zsh
 chsh -s $(which zsh)
 
